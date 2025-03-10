d624ea02ea5979e1364545879079ee2540c59c94
neilsoneja <93364375+neilsoneja@users.noreply.github.com>
2025-03-10 18:05:22 +0800

    Update main.py

--- PD_project/pages/main.py
+++ PD_project/pages/main.py
@@ -32,6 +32,8 @@ class DashboardPage(Frame):
 
         try:
             # Initialize hardware and components
+            # Pins(varpin3, varpin2, varpin1, actuator1, actuator2, enable, start)
+            #pins to arduino = varpin3, varpin2, varpin1, enable, start
             self.pins = Pins(14, 15, 18, 23, 24, 22, 27)
             self.pins.all_pin_low()
             self.stepper = Stepper(11, 9, 10)
